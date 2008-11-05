1a4269c71efedceb120d06f4f83e07803dfd7bc2
Will Norris <will@willnorris.com>
2008-11-05 15:59:22 -0800

    cleanup code

--- comment-saver.php
+++ comment-saver.php
@@ -8,53 +8,58 @@ Version: trunk
 License: Dual GPL (http://www.fsf.org/licensing/licenses/info/GPLv2.html) and Modified BSD (http://www.fsf.org/licensing/licenses/index_html#ModifiedBSD)
 */
 
-if  ( !class_exists('CommentSaver') ) {
-	class CommentSaver {
 
-		/** Get path for cookie. */
-		function get_path() {
-			$url_bits = parse_url(get_option('home'));
-			$path = $url_bits['path'];
-			return $path ? $path : '/';
-		}
+add_action('comment_form', 'comment_saver_form');
+add_filter('comment_post_redirect', 'comment_saver_cleanup', 10, 2);
+add_action('wp_head', 'comment_saver_js', 9);
 
-		/** Setup require javascript. */
-		function js_setup() {
-			if (is_single() || is_comments_popup()) {
-				wp_enqueue_script( 'jquery' );
-				wp_enqueue_script('jquery.cookie', plugins_url('comment-saver') . '/jquery.cookie.min.js', array('jquery'));
-			}
-		}
 
-		/** Add jQuery actions to save and restore comment. */
-		function comment_form($id) { 
-			$cookieName = 'comment_saver_post' . $id;
-			$path = CommentSaver::get_path();
-?>
-			<script type="text/javascript">
-				jQuery('#commentform').submit(function() {
-					jQuery.cookie('<?php echo $cookieName;?>', jQuery('#comment').val(), {expires: (1/24), path: '<?php echo $path;?>'});
-				});
-				if (jQuery('#comment').val() == '') {
-					jQuery('#comment').val(jQuery.cookie('<?php echo $cookieName;?>'));
-				}
-			</script>
-		<?php }
+/** 
+ * Get path for comment saver cookie. 
+ */
+function comment_saver_cookie_path() {
+	$parts = parse_url(get_option('home'));
+	$path = $parts['path'];
+	return $path ? $path : '/';
+}
 
-		function remove_cookie($location, $comment) {
-			$path = CommentSaver::get_path();
 
-			setcookie('comment_saver_post' . $comment->comment_post_ID, null, -1, $path);
-			return $location;
-		}
-
+/** 
+ * Setup require javascript. 
+ */
+function comment_saver_js() {
+	if (is_single() || is_comments_popup()) {
+		wp_enqueue_script('jquery.cookie', plugins_url('comment-saver') . '/jquery.cookie.min.js', array('jquery'));
 	}
 }
 
-if (isset($wp_version)) {
-	add_action( 'comment_form', array( 'CommentSaver', 'comment_form'));
-	add_filter( 'comment_post_redirect', array( 'CommentSaver', 'remove_cookie'), 10, 2);
-	add_action( 'wp_head', array( 'CommentSaver', 'js_setup'), 9);
+
+/** 
+ * Add jQuery actions to save and restore comment. 
+ */
+function comment_saver_form($id) { 
+	$cookieName = 'comment_saver_post' . $id;
+	$path = comment_saver_cookie_path();
+
+	echo '
+	<script type="text/javascript">
+		jQuery("#commentform").submit(function() {
+			jQuery.cookie("' . $cookieName . '", jQuery("#comment").val(), {expires: (1/24), path: "' . $path . '"});
+		});
+		if (jQuery("#comment").val() == "") {
+			jQuery("#comment").val(jQuery.cookie("' . $cookieName . '"));
+		}
+	</script>';
+}
+
+
+/**
+ * Cleanup comment saver cookie.
+ */
+function comment_saver_cleanup($location, $comment) {
+	$path = comment_saver_cookie_path();
+	setcookie('comment_saver_post' . $comment->comment_post_ID, null, -1, $path);
+	return $location;
 }
 
 ?>
