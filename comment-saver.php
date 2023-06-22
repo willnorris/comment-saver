e703bdaefa45d8fd6d6d50829d522ebb00a0c22b
Mészáros Róbert <hmm@meszarosrob.com>
2023-06-22 07:59:10 +0300

    Remove deprecated is_comments_popup check
    This returns false since WP version 4.5 https://github.com/WordPress/WordPress/blob/98ec0401b6491625aa4fd50feb67f4d595c6b255/wp-includes/deprecated.php#L3734-L3750
    
    In theory, this is a breaking change, as everybody using some ancient WP will lose this feature.

--- comment-saver.php
+++ comment-saver.php
@@ -28,7 +28,7 @@ function comment_saver_cookie_path() {
  * Setup require javascript.
  */
 function comment_saver_js() {
-	if ( is_single() || is_comments_popup() ) {
+	if ( is_single() ) {
 		wp_enqueue_script( 'jquery.cookie', plugins_url( 'comment-saver/jquery.cookie.min.js' ), array( 'jquery' ), '1.0', true );
 	}
 }
