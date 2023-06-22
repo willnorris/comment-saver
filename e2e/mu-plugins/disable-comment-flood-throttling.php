a780c440238075dc34ef14b5f216f17653c582a1
Mészáros Róbert <hmm@meszarosrob.com>
2023-06-22 20:08:12 +0300

    Update doc formatting of the E2E mu-plugin to pass the WP PHPCS
    While this is really unnecessary...

--- e2e/mu-plugins/disable-comment-flood-throttling.php
+++ e2e/mu-plugins/disable-comment-flood-throttling.php
@@ -1,5 +1,11 @@
 <?php
+/**
+ * Disables the comment flood throttling
+ *
+ * When running the E2E this, we could post comments quickly.
+ * https://github.com/WordPress/WordPress/blob/84e9601e5a2966c0aa80020bbf0c043dd8b6bfbb/wp-includes/default-filters.php#L299
+ *
+ * @package comment-saver-e2e-test
+ */
 
-// When running the E2E tests, we know that we are flooding!
-// https://github.com/WordPress/WordPress/blob/84e9601e5a2966c0aa80020bbf0c043dd8b6bfbb/wp-includes/default-filters.php#L299
 remove_filter( 'comment_flood_filter', 'wp_throttle_comment_flood', 10 );
